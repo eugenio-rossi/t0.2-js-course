4e1e571f17a237957860d95b4ff64eaaff828403
eugenio-rossi <eugeniotrossi@gmail.com>
2021-02-28 20:19:40 +0100

    test

--- custom.js
+++ custom.js
@@ -1,10 +1,29 @@
 console.log('connected');
 
-const lista = document.getElementById('lista');
-const listaValue = lista.nodeValue;
+const sezioneLista = document.getElementById("sezione-lista");
 
-if(!localStorage.getItem('listaValue')) {
-    populateStorage();
-  } else {
-    setStyles();
-  }
+function createList() {
+  const elementoLista = document.getElementById('elementoLista');
+  const listaValue = elementoLista.value;
+
+  let ulLista = document.getElementById("ul-lista");
+  ulLista.innerHTML += `<li>${listaValue}</li>`;
+
+}
+
+function saveList(){
+
+  let listedElements = document.getElementById("ul-lista").getElementsByTagName("li");
+  // trasformo la collection HTML in array per semplicità
+  let listedArray = Array.from(listedElements);
+  listedArray.forEach(element => {
+    alert(element.innerHTML);
+  });
+}
+
+
+// Step:
+// #1: Salvare a WebStorage L'elemento
+// #1.1: OnSubmit del button, salvare a variabile il valore dell'input e inserirlo in un array nel web storage.
+// #2: Creare una nuova Lista
+// #3: Colorare gli elmenti
