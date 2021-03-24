9b40da728d0e7168f2f9e7172c294c67d5c97379
eugenio-rossi <eugeniotrossi@gmail.com>
2021-03-24 09:05:32 +0100

    started commit with crocco

--- custom.js
+++ custom.js
@@ -2,15 +2,12 @@
 
 // Declaration of Variabiles & Object
 
-let list = [{
+let list = {
   id : Math.round((Math.random() * 100)),
   title : "Title",
   color : "#ececec",
   taskList : [] 
-}];
-
-// Since it is a JSON like obj, I have a varible to bypass calling the index anytime
-aList = list[0];
+};
 
 // Get the input of Tasks
 const addToList = document.getElementById("addToList");
@@ -29,10 +26,10 @@ function CreateList(){
   sectionListedElements.innerHTML = "";
     
   // Recreate the html structure from the object values each time ad item is added
-  aList.taskList.forEach(element => {
+  list.taskList.forEach(element => {
       sectionListedElements.innerHTML += "<li>" + element;
       // the function removeTask has the index of the element as input to remove it from DOM & from list
-      sectionListedElements.innerHTML += `<button onclick="removeTask(`+ aList.taskList.indexOf(element) + `)" id="removeTitleToList"><span class="lnr lnr-cross-circle"></span></button>`;
+      sectionListedElements.innerHTML += `<button onclick="removeTask(`+ list.taskList.indexOf(element) + `)" id="removeTitleToList"><span class="lnr lnr-cross-circle"></span></button>`;
       sectionListedElements.innerHTML += "</li>";
   });
 }
@@ -42,9 +39,9 @@ function addTaskToList() {
   let listTask = document.getElementById("listTask").value;
 
   // Che is the value of the task is populated & if it is not already in the list
-  if ((listTask) && !(aList.taskList.includes(listTask))) {
+  if ((listTask) && !(list.taskList.includes(listTask))) {
     // Push the value in the object
-    aList.taskList.push(listTask);
+    list.taskList.push(listTask);
 
     // DOM part of the function
     CreateList();
@@ -54,7 +51,7 @@ function addTaskToList() {
 
 
 function removeTask(index){
-  aList.taskList.splice(index, 1);
+  list.taskList.splice(index, 1);
   CreateList();
 }
 
@@ -62,7 +59,7 @@ function validateTitle() {
 
   // Logical part of the function
   const title = document.getElementById("title").value;
-  aList.title = title;
+  list.title = title;
 
   // DOM part of the function
   sectionListedTitle = document.getElementById("sectionListedTitle");
@@ -74,7 +71,7 @@ function validateColor() {
 
   // Logical part of the function
   const color = document.getElementById("color").value;
-  aList.color = color;
+  list.color = color;
 
   // DOM part of the function
   cardBody.style.backgroundColor = color;
@@ -94,10 +91,10 @@ function removeValue(element) {
 }
 
 function restartList(arg) {
-  arg[0].id = Math.round((Math.random() * 100));
-  arg[0].title = "Title";
-  arg[0].color = "#ececec";
-  arg[0].taskList = [];
+  arg.id = Math.round((Math.random() * 100));
+  arg.title = "Title";
+  arg.color = "#ececec";
+  arg.taskList = [];
 
   document.getElementById("sectionListedElements").innerHTML = "";
   document.getElementById("sectionListedTitle").innerHTML = "";
@@ -116,10 +113,6 @@ function createSavedItem(){
 function saveList(){
   let jList = JSON.stringify(list);
   if (localStorage.getItem("listItem") === null){
-    listToPush = [];
-    listToPush.push(jList);
-    jList = listToPush;
-    console.log(jList);
     localStorage.setItem("listItem", jList);
   }
   else {
