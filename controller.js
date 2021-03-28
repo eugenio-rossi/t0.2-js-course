9cf3eb0769154115ffb3efe00628148d1b203e4d
eugenio-rossi <eugeniotrossi@gmail.com>
2021-03-28 17:26:14 +0200

    Started the controller

---
+++ controller.js
@@ -0,0 +1,37 @@
+// Starting Over
+
+// Declaring all methods for lists
+
+// Function to create List
+function createList(list){
+    // Check all elements of the List object
+    if (list.title !== "" && list.color !== "" && list.tasks.lenght > 0) {
+      // Check if local storage contains our object
+      var lists = localStorage.getItem("Lists");
+  
+      if (lists) {
+        
+      }
+      else {
+        
+      }
+  
+    }
+  }
+  
+  // Function to delete List
+  function deleteList(list) {
+  
+  }
+  
+  // Function to add item to List
+  function addItemList(item, list) {
+  
+  }
+  
+  // Function to add remove tem to List
+  function removeItemList(item, list) {
+  
+  }
+  
+  
